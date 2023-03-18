c4d49cdbfb60a1dbde2f9dff115c8695e2ca3f27
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2023-03-18 11:41:48 -0600

    pseudocodigo ejercicio3

--- semestre1/programacion/parcial1/practica1_ft_marin/ejercicio2.c
+++ semestre1/programacion/parcial1/practica1_ft_marin/ejercicio2.c
@@ -1,3 +1,38 @@
+/*
+Pseudocodigo:
+
+Entrada: Real a, b, c, d, e, f
+Salida: x, y,
+
+Imprimir "Escribe el coeficiente a: "
+Leer a
+
+Imprimir "Escribe el coeficiente b: "
+Leer b
+
+Imprimir "Escribe el resultado de la primera ecuación (c): "
+Leer c
+
+Imprimir "Escribe el coeficiente d: "
+Leer d
+
+Imprimir "Escribe el coeficiente e: "
+Leer e
+
+Imprimir "Escribe el resultado de la segunda ecuación (f): "
+Leer f
+
+Si ((a * e) - (b * d) == 0) entonces
+    Imprimir "La ecuación dada no tiene soluciones"
+sino
+    x = ((c * e) - (b * f)) / ((a * e) - (b * d))
+    y = ((a * f) - (c * d)) / ((a * e) - (b * d))
+    Imprimir "El valor de x es: " concatenar x
+    Imprimir "El valor de y es: " concatenar y
+finSi
+
+*/
+
 #include <stdio.h>
 
 int main() {
