80fa15bfa0c7797df2d6ec142d28b0112921cb5f
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2023-09-27 21:18:54 -0600

    27sep

---
+++ semestre2/algoritmos_y_ED/apuntes/sep27/ordenamiento.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 1000
+
+int* crearArreglo() {
+    int* arreglo = calloc(TAM, sizeof(int));
+    srand(time(NULL));
+    if (arreglo == NULL) {
+        printf("No se pudo crear el arreglo");
+        exit(1);
+    }
+    for (int i = 0; i < TAM; i++) {
+        arreglo[i] = (rand() % 100) - 1;
+    }
+
+    return arreglo;
+}
+
+/*
+    Función que destruye un arreglo
+    @param arreglo: Un arreglo de enteros
+    @return nada
+*/
+void destruirArreglo(int* arreglo) {
+    free(arreglo);
+}
+
+void ordenaSeleccion(int* arreglo) {
+    int aux = 0;
+    for (int i = 0; i < TAM; i++) {
+        for (int j = i + 1; j < TAM; j++) {
+            if (arreglo[j] > arreglo[i]) {
+                aux = arreglo[i];
+                arreglo[i] = arreglo[j];
+                arreglo[j] = aux;
+            }
+        }
+    }
+}
+
+// Todo lo que sea consulta, se pasa por valor y no referencia para no modificar los datos
+
+void imprimeArreglo(int arreglo[TAM]) {
+    for (int k = 0; k < TAM; k++) {
+        printf("Arreglo[%d] = %d\n", k, arreglo[k]);
+    }
+}
+
+void main() {
+    puts("Bienvenido");
+    int* arreglo = crearArreglo();
+    puts("Arreglo original");
+    imprimeArreglo(arreglo);
+    ordenaSeleccion(arreglo);
+    puts("Arreglo ordenado");
+    imprimeArreglo(arreglo);
+    destruirArreglo(arreglo);
+    puts("Fin del programa");
+}
